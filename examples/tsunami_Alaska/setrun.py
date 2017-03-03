ffdfc4c3002c4352cc3d0050f2fc76793dda28bf
Randy LeVeque <rjl@uw.edu>
2017-03-03 13:05:45 -0800

    make references to scratch directory for topo relative

--- examples/tsunami_Alaska/setrun.py
+++ examples/tsunami_Alaska/setrun.py
@@ -20,6 +20,7 @@ except:
 
 # Scratch directory for storing topo and dtopo files:
 scratch_dir = os.path.join(CLAW, 'adjoint', 'scratch')
+scratch_dir = os.path.abspath('../../scratch')
 
 #------------------------------
 def setrun(claw_pkg='geoclaw'):
